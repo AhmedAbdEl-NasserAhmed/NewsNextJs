c762c5e2784689829726d11aab70b49234e6d5ce
Ahmed Nasser <nasserahmed093@gmail.com>
2024-05-20 03:18:53 +0300

    adding suspense

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,11 +1,18 @@
 import NewsList from "@/components/NewsList/NewsList";
-import { DUMMY_NEWS } from "@/dummy-news";
 
-function NewsPage() {
+async function NewsPage() {
+  const response = await fetch("http://localhost:8080/news");
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch News");
+  }
+
+  const news = await response.json();
+
   return (
     <>
       <h1>News Details Page</h1>
-      <NewsList news={DUMMY_NEWS} />
+      <NewsList news={news} />
     </>
   );
 }
--- components/NewsList/NewsList.js
+++ components/NewsList/NewsList.js
@@ -4,7 +4,7 @@ function NewsList({ news }) {
   return (
     <ul className="news-list">
       {news.map((newsItem) => (
-        <li>
+        <li key={newsItem.slug}>
           <Link href={`/news/${newsItem.slug}`}>
             <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
             <span>{newsItem.title}</span>
